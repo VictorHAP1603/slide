32b38a06624f966e68d4f31e4d8610b0e7e63fe3
Victor Hugo <valvesperricci@yahoo.com.br>
2021-02-05 03:57:47 -0300

    calcula de distancia quando o clique ocorre

--- js/slide.js
+++ js/slide.js
@@ -4,17 +4,35 @@ export default class Slide {
   constructor(wrapper, slide) {
     this.wrapper = document.querySelector(wrapper);
     this.slide = document.querySelector(slide);
+
+    this.dist = { finalPostion: 0, startX: 0, movement: 0 };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePostion(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPostion - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
-  onMove() {}
+  onMove(event) {
+    const finalPositon = this.updatePostion(event.clientX);
+    this.moveSlide(finalPositon);
+  }
 
   onEnd() {
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPostion = this.dist.movePosition;
   }
 
   addSlideEvents() {
