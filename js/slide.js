f8891272495d72286586de294ec5f100790141c2
Victor Hugo <valvesperricci@yahoo.com.br>
2021-02-05 03:36:27 -0300

    esqueleto dos eventos

---
+++ js/slide.js
@@ -0,0 +1,35 @@
+import debounce from "./debounce.js";
+
+export default class Slide {
+  constructor(wrapper, slide) {
+    this.wrapper = document.querySelector(wrapper);
+    this.slide = document.querySelector(slide);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
+
+  onMove() {}
+
+  onEnd() {
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+  }
+}
